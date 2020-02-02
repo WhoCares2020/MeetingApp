f94e9c320fb763a9f2f47c94a042163a8c55e7c9
malhotraguy <r_malhot@encs.concordia.ca>
2020-02-02 05:41:47 -0500

    route to start real time transcript

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, request
 
+from backend.audio_stream_test import start_stream_transcription
+
 app = Flask(__name__)
+
 
 @app.route('/startrecord', methods=['POST'])
 def startrecord():
     print("START")
+    start_stream_transcription()
+
 
 @app.route('/stoprecord', methods=['POST'])
 def stoprecord():
