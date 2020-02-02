6900ea8b563e8681a2e7fe47935b4047c663bd7a
Sheheryar Parvaz <skipper308@hotmail.ca>
2020-02-01 20:49:12 -0500

    Add firebase and material UI deps

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
+// import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import { db } from "./utils/firebase";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    db.collection("meetings")
+      .get()
+      .then(data =>
+        data.forEach(x => {
+          setUsers(x.data().attendees);
+        })
+      );
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
+      {users.map(a => (
+        <p>{a}</p>
+      ))}
     </div>
   );
 }
